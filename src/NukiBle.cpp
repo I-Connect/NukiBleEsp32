7803b06b266b2da7c23a1a5e9556d1c32f10dc72
Jeroen <Jeroen@i-connect.online>
2020-10-20 16:28:08 +0200

    working step 14 (send correct authenticator)

--- src/NukiBle.cpp
+++ src/NukiBle.cpp
@@ -8,9 +8,15 @@
 #include "NukiBle.h"
 #include "Crc16.h"
 #include "string.h"
-#include "endian.h"
+#include "sodium/crypto_scalarmult.h"
+#include "sodium/crypto_core_hsalsa20.h"
+#include "sodium/crypto_auth_hmacsha256.h"
+
+unsigned char remotePublicKey[32] = {0x00, 0x00, 0x0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+unsigned char challengeData[32] = {0x00, 0x00, 0x0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
 void printBuffer(const byte* buff, const uint8_t size, const boolean asChars, const char* header) {
+  delay(100); //delay otherwise first part of print will not be shown 
   if (strlen(header) > 0) {
     Serial.print(header);
     Serial.print(": ");
@@ -50,9 +56,8 @@ void NukiBle::initialize() {
   #endif
   BLEDevice::init("ESP32_test");
 
-  //generate public and private keys
-  keyGen(private_key, 32, 34);
-  keyGen(public_key, 32, 34);
+  // keyGen(localPrivate_key, 32, 34);
+  // keyGen(localPublic_key, 32, 34);
 }
 
 void NukiBle::startNukiBleXtask(){
@@ -76,11 +81,57 @@ bool NukiBle::connect() {
     return false;
   }
 
-  //send public key command (Sent message should be 0100030027A7)
+  //Request remote public key (Sent message should be 0100030027A7)
+  log_d("##################### REQUEST REMOTE PUBLIC KEY #########################");
   uint16_t payload = (uint16_t)nukiCommand::publicKey;
   sendPlainMessage(nukiCommand::requestData, (char*)&payload, sizeof(payload));
+  log_d("#########################################################################");
 
-  log_d("BLE connect and pairing success");
+  //TODO generate public and private keys
+  static unsigned char myPrivateKey[32] = {0x8C, 0xAA, 0x54, 0x67, 0x23, 0x07, 0xBF, 0xFD, 0xF5, 0xEA, 0x18, 0x3F, 0xC6, 0x07, 0x15, 0x8D, 0x20, 0x11, 0xD0, 0x08, 0xEC, 0xA6, 0xA1, 0x08, 0x86, 0x14, 0xFF, 0x08, 0x53, 0xA5, 0xAA, 0x07};
+  static unsigned char myPublicKey[32] = {0xF8, 0x81, 0x27, 0xCC, 0xF4, 0x80, 0x23, 0xB5, 0xCB, 0xE9, 0x10, 0x1D, 0x24, 0xBA, 0xA8, 0xA3, 0x68, 0xDA, 0x94, 0xE8, 0xC2, 0xE3, 0xCD, 0xE2, 0xDE, 0xD2, 0x9C, 0xE9, 0x6A, 0xB5, 0x0C, 0x15};
+
+  //simulated test keys
+  // static unsigned char remotePublicKeySim[32] = {0x39, 0xE7, 0x5F, 0xB9, 0x4B, 0xBE, 0xC0, 0x31, 0x7F, 0x8C, 0xFB, 0x88, 0x09, 0xEB, 0x50, 0x97, 0x51, 0x45, 0x29, 0x83, 0x35, 0xC0, 0xE1, 0x26, 0xCD, 0x48, 0xCB, 0x64, 0xB5, 0x7C, 0x8C, 0x26};
+  // static unsigned char challengeDataSim[32] = {0xe8, 0x8d, 0xf6, 0x06, 0x0d, 0x05, 0x72, 0x71, 0xec, 0x70, 0x27, 0x90, 0xc7, 0x77, 0x0c, 0xf5, 0xa1, 0xa4, 0x56, 0x68, 0xe1, 0xc2, 0x6d, 0xb3, 0xfd, 0x2e, 0x50, 0xbd, 0x2c, 0x25, 0x6b, 0xfb};
+
+  log_d("##################### SEND CLIENT PUBLIC KEY #########################");
+  sendPlainMessage(nukiCommand::publicKey, (char*)&myPublicKey, sizeof(myPublicKey));
+  log_d("######################################################################");
+
+  log_d("##################### CALCULATE DH SHARED KEY s #########################");
+  unsigned char sharedKeyS[32];
+  int i= crypto_scalarmult_curve25519(sharedKeyS, myPrivateKey, remotePublicKey);
+  printBuffer(sharedKeyS, sizeof(sharedKeyS), false, "Shared key s");
+  log_d("######################################################################");
+
+  log_d("##################### DERIVE LONG TERM SECRET KEY k #########################");
+  unsigned char secretKeyK[32];
+  unsigned char _0[16];
+  memset(_0, 0, 16);
+  unsigned char sigma[] = "expand 32-byte k";
+  int y= crypto_core_hsalsa20(secretKeyK, _0, sharedKeyS, sigma);
+  printBuffer(secretKeyK, sizeof(secretKeyK), false, "Secret key k");
+  log_d("######################################################################");
+
+  log_d("##################### CALCULATE/VERIFY AUTHENTICATOR #########################");
+  //concatenate local public key, remote public key and receive challenge data
+  unsigned char authenticator[32];
+  unsigned char hmacPayload[96];
+  memcpy(&hmacPayload[0], myPublicKey, sizeof(myPublicKey));
+  memcpy(&hmacPayload[32], remotePublicKey, sizeof(remotePublicKey));
+  memcpy(&hmacPayload[64], challengeData, sizeof(challengeData));
+  printBuffer((byte*)hmacPayload, sizeof(hmacPayload), false, "Concatenated data r");
+  crypto_auth_hmacsha256(authenticator, hmacPayload, sizeof(hmacPayload), secretKeyK);
+  printBuffer(authenticator, sizeof(authenticator), false, "HMAC 256 result");
+  log_d("######################################################################");
+
+  log_d("##################### SEND AUTHENTICATOR #########################");
+  sendPlainMessage(nukiCommand::authorizationAuthenticator, (char*)&authenticator, sizeof(authenticator));
+  log_d("######################################################################");
+
+
+  // log_d("BLE connect and pairing success");
   return true;
 }
 
@@ -115,9 +166,10 @@ void NukiBle::sendPlainMessage(nukiCommand commandIdentifier, char* payload, uin
   dataCrc = crcObj.fastCrc((uint8_t*)dataToSend, 0, payloadLen + 2, false, false, 0x1021, 0xffff, 0x0000, 0x8000, 0xffff);
   
   memcpy(&dataToSend[2+payloadLen], &dataCrc, sizeof(dataCrc));
-  log_d("Sending plain message %02x%02x%02x%02x%02x%02x", dataToSend[0], dataToSend[1], dataToSend[2], dataToSend[3], dataToSend[4] , dataToSend[5]);
+  printBuffer((byte*)dataToSend, payloadLen + 4, false, "Sending plain message");
   log_d("Command identifier: %02x, CRC: %04x", (uint32_t)commandIdentifier, dataCrc);
   pGdioCharacteristic->writeValue((uint8_t*)dataToSend, payloadLen + 4, true);
+  delay(1000); //wait for response via BLE char
 }
 
 bool NukiBle::executeLockAction(lockAction aLockAction) {
@@ -127,7 +179,6 @@ bool NukiBle::executeLockAction(lockAction aLockAction) {
 
 void NukiBle::notifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify) {
   // log_d(" Notify callback for characteristic: %s of length: %d", pBLERemoteCharacteristic->getUUID().toString().c_str(), length);
-  delay(100);
   printBuffer((byte*)pData, length, false, "Received data");
 
   //check CRC
@@ -138,20 +189,22 @@ void NukiBle::notifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, 
   dataCrc = crcObj.fastCrc(pData, 0, length - 2 , false, false, 0x1021, 0xffff, 0x0000, 0x8000, 0xffff);
   // log_d("Received CRC: %d, calculated CRC: %d", receivedCrc, dataCrc);
   uint16_t returnCode = ((uint16_t)pData[1] << 8) | pData[0];
-  log_d("Return code: %d", returnCode);
+  // log_d("Return code: %d", returnCode);
   if(!(receivedCrc == dataCrc)){
     log_e("CRC CHECK FAILED!");
+    //TODO retry last communications
   }
   else{
-    log_d("CRC CHECK OKE");
-  }
-
-  if(returnCode == (uint16_t)nukiCommand::errorReport){
-    // log_e("ERROR: %02x", pData[2]);
-    handleErrorCode(pData[2]);
-  }
-  else{
-    handleReturnCode(returnCode);
+    // log_d("CRC CHECK OKE");
+    if(returnCode == (uint16_t)nukiCommand::errorReport){
+      // log_e("ERROR: %02x", pData[2]);
+      handleErrorCode(pData[2]);
+    }
+    else{
+      char data[100];
+      memcpy(data, &pData[2], length - 4);
+      handleReturnMessage(returnCode, data, length - 4);
+    }
   }
 }
 
@@ -271,164 +324,166 @@ void NukiBle::handleErrorCode(uint8_t errorCode){
     }
 }
 
-void NukiBle::handleReturnCode(uint16_t returnCode){
-  
+void NukiBle::handleReturnMessage(uint16_t returnCode, char* data, uint8_t dataLen){
+
   switch(returnCode) {
     case (uint16_t)nukiCommand::requestData :
-      log_e("requestData");
+      log_d("requestData");
       break;
     case (uint16_t)nukiCommand::publicKey :
-      log_e("publicKey");
+      memcpy(remotePublicKey, data, 32);
+      printBuffer(remotePublicKey, sizeof(remotePublicKey), false,  "Remote public key");
       break;
     case (uint16_t)nukiCommand::challenge :
-      log_e("challenge");
+      memcpy(challengeData, data, 32);
+      printBuffer((byte*)data, dataLen, false, "Challenge");
       break;
     case (uint16_t)nukiCommand::authorizationAuthenticator :
-      log_e("authorizationAuthenticator");
+      printBuffer((byte*)data, dataLen, false, "authorizationAuthenticator");
       break;
     case (uint16_t)nukiCommand::authorizationData :
-      log_e("authorizationData");
+      printBuffer((byte*)data, dataLen, false, "authorizationData");
       break;
     case (uint16_t)nukiCommand::authorizationId :
-      log_e("authorizationId");
+      printBuffer((byte*)data, dataLen, false, "authorizationId");
       break;
     case (uint16_t)nukiCommand::removeUserAuthorization :
-      log_e("removeUserAuthorization");
+      printBuffer((byte*)data, dataLen, false, "removeUserAuthorization");
       break;
     case (uint16_t)nukiCommand::requestAuthorizationEntries :
-      log_e("requestAuthorizationEntries");
+      printBuffer((byte*)data, dataLen, false, "requestAuthorizationEntries");
       break;
     case (uint16_t)nukiCommand::authorizationEntry :
-      log_e("authorizationEntry");
+      printBuffer((byte*)data, dataLen, false, "authorizationEntry");
       break;
     case (uint16_t)nukiCommand::authorizationDatInvite :
-      log_e("authorizationDatInvite");
+      printBuffer((byte*)data, dataLen, false, "authorizationDatInvite");
       break;
     case (uint16_t)nukiCommand::keyturnerStates :
-      log_e("keyturnerStates");
+      printBuffer((byte*)data, dataLen, false, "keyturnerStates");
       break;
     case (uint16_t)nukiCommand::lockAction :
-      log_e("lockAction");
+      printBuffer((byte*)data, dataLen, false, "lockAction");
       break;
     case (uint16_t)nukiCommand::status :
-      log_e("status");
+      printBuffer((byte*)data, dataLen, false, "status");
       break;
     case (uint16_t)nukiCommand::mostRecentCommand :
-      log_e("mostRecentCommand");
+      printBuffer((byte*)data, dataLen, false, "mostRecentCommand");
       break;
     case (uint16_t)nukiCommand::openingsClosingsSummary :
-      log_e("openingsClosingsSummary");
+      printBuffer((byte*)data, dataLen, false, "openingsClosingsSummary");
       break;
     case (uint16_t)nukiCommand::batteryReport :
-      log_e("batteryReport");
+      printBuffer((byte*)data, dataLen, false, "batteryReport");
       break;
     case (uint16_t)nukiCommand::errorReport :
-      log_e("errorReport");
+      printBuffer((byte*)data, dataLen, false, "errorReport");
       break;
     case (uint16_t)nukiCommand::setConfig :
-      log_e("setConfig");
+      printBuffer((byte*)data, dataLen, false, "setConfig");
       break;
     case (uint16_t)nukiCommand::requestConfig :
-      log_e("requestConfig");
+      printBuffer((byte*)data, dataLen, false, "requestConfig");
       break;
     case (uint16_t)nukiCommand::config :
-      log_e("config");
+      printBuffer((byte*)data, dataLen, false, "config");
       break;
     case (uint16_t)nukiCommand::setSecurityPin :
-      log_e("setSecurityPin");
+      printBuffer((byte*)data, dataLen, false, "setSecurityPin");
       break;
     case (uint16_t)nukiCommand::requestCalibration :
-      log_e("requestCalibration");
+      printBuffer((byte*)data, dataLen, false, "requestCalibration");
       break;
     case (uint16_t)nukiCommand::requestReboot :
-      log_e("requestReboot");
+      printBuffer((byte*)data, dataLen, false, "requestReboot");
       break;
     case (uint16_t)nukiCommand::authorizationIdConfirmation :
-      log_e("authorizationIdConfirmation");
+      printBuffer((byte*)data, dataLen, false, "authorizationIdConfirmation");
       break;
     case (uint16_t)nukiCommand::authorizationIdInvite :
-      log_e("authorizationIdInvite");
+      printBuffer((byte*)data, dataLen, false, "authorizationIdInvite");
       break;
     case (uint16_t)nukiCommand::verifySecurityPin :
-      log_e("verifySecurityPin");
+      printBuffer((byte*)data, dataLen, false, "verifySecurityPin");
       break;
     case (uint16_t)nukiCommand::updateTime :
-      log_e("updateTime");
+      printBuffer((byte*)data, dataLen, false, "updateTime");
       break;
     case (uint16_t)nukiCommand::updateUserAuthorization :
-      log_e("updateUserAuthorization");
+      printBuffer((byte*)data, dataLen, false, "updateUserAuthorization");
       break;
     case (uint16_t)nukiCommand::authorizationEntryCount :
-      log_e("authorizationEntryCount");
+      printBuffer((byte*)data, dataLen, false, "authorizationEntryCount");
       break;
     case (uint16_t)nukiCommand::requestLogEntries :
-      log_e("requestLogEntries");
+      printBuffer((byte*)data, dataLen, false, "requestLogEntries");
       break;
     case (uint16_t)nukiCommand::logEntry :
-      log_e("logEntry");
+      printBuffer((byte*)data, dataLen, false, "logEntry");
       break;
     case (uint16_t)nukiCommand::logEntryCount :
-      log_e("logEntryCount");
+      printBuffer((byte*)data, dataLen, false, "logEntryCount");
       break;
     case (uint16_t)nukiCommand::enableLogging :
-      log_e("enableLogging");
+      printBuffer((byte*)data, dataLen, false, "enableLogging");
       break;
     case (uint16_t)nukiCommand::setAdvancedConfig :
-      log_e("setAdvancedConfig");
+      printBuffer((byte*)data, dataLen, false, "setAdvancedConfig");
       break;
     case (uint16_t)nukiCommand::requestAdvancedConfig :
-      log_e("requestAdvancedConfig");
+      printBuffer((byte*)data, dataLen, false, "requestAdvancedConfig");
       break;
     case (uint16_t)nukiCommand::advancedConfig :
-      log_e("advancedConfig");
+      printBuffer((byte*)data, dataLen, false, "advancedConfig");
       break;
     case (uint16_t)nukiCommand::addTimeControlEntry :
-      log_e("addTimeControlEntry");
+      printBuffer((byte*)data, dataLen, false, "addTimeControlEntry");
       break;
     case (uint16_t)nukiCommand::timeControlEntryId :
-      log_e("timeControlEntryId");
+      printBuffer((byte*)data, dataLen, false, "timeControlEntryId");
       break;
     case (uint16_t)nukiCommand::removeTimeControlEntry :
-      log_e("removeTimeControlEntry");
+      printBuffer((byte*)data, dataLen, false, "removeTimeControlEntry");
       break;
     case (uint16_t)nukiCommand::requestTimeControlEntries :
-      log_e("requestTimeControlEntries");
+      printBuffer((byte*)data, dataLen, false, "requestTimeControlEntries");
       break;
     case (uint16_t)nukiCommand::timeControlEntryCount :
-      log_e("timeControlEntryCount");
+      printBuffer((byte*)data, dataLen, false, "timeControlEntryCount");
       break;
     case (uint16_t)nukiCommand::timeControlEntry :
-      log_e("timeControlEntry");
+      printBuffer((byte*)data, dataLen, false, "timeControlEntry");
       break;
     case (uint16_t)nukiCommand::updateTimeControlEntry :
-      log_e("updateTimeControlEntry");
+      printBuffer((byte*)data, dataLen, false, "updateTimeControlEntry");
       break;
     case (uint16_t)nukiCommand::addKeypadCode :
-      log_e("addKeypadCode");
+      printBuffer((byte*)data, dataLen, false, "addKeypadCode");
       break;
     case (uint16_t)nukiCommand::keypadCodeId :
-      log_e("keypadCodeId");
+      printBuffer((byte*)data, dataLen, false, "keypadCodeId");
       break;
     case (uint16_t)nukiCommand::requestKeypadCodes :
-      log_e("requestKeypadCodes");
+      printBuffer((byte*)data, dataLen, false, "requestKeypadCodes");
       break;
     case (uint16_t)nukiCommand::keypadCodeCount :
-      log_e("keypadCodeCount");
+      printBuffer((byte*)data, dataLen, false, "keypadCodeCount");
       break;
     case (uint16_t)nukiCommand::keypadCode :
-      log_e("keypadCode");
+      printBuffer((byte*)data, dataLen, false, "keypadCode");
       break;
     case (uint16_t)nukiCommand::updateKeypadCode :
-      log_e("updateKeypadCode");
+      printBuffer((byte*)data, dataLen, false, "updateKeypadCode");
       break;
     case (uint16_t)nukiCommand::removeKeypadCode :
-      log_e("removeKeypadCode");
+      printBuffer((byte*)data, dataLen, false, "removeKeypadCode");
       break;
     case (uint16_t)nukiCommand::keypadAction :
-      log_e("keypadAction");
+      printBuffer((byte*)data, dataLen, false, "keypadAction");
       break;
     case (uint16_t)nukiCommand::simpleLockAction :
-      log_e("simpleLockAction");
+      printBuffer((byte*)data, dataLen, false, "simpleLockAction");
       break;
     default:
       log_e("UNKNOWN RETURN COMMAND");
--- src/NukiBle.h
+++ src/NukiBle.h
@@ -29,9 +29,6 @@ class NukiBle : public BLEClientCallbacks{
       isPaired = status;
     }
 
-    static void handleErrorCode(uint8_t errorCode);
-    static void handleReturnCode(uint16_t returnCode);
-
     bool executeLockAction(lockAction action);
 
   private:
@@ -45,6 +42,8 @@ class NukiBle : public BLEClientCallbacks{
     void sendPlainMessage(nukiCommand commandIdentifier, char* payload, uint8_t payloadLen);
     static void notifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify);
     static void my_gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t* param);
+    static void handleErrorCode(uint8_t errorCode);
+    static void handleReturnMessage(uint16_t returnCode, char* data, uint8_t dataLen);
         
     std::string bleAddress = "";
     BLEClient* pClient;
@@ -52,8 +51,8 @@ class NukiBle : public BLEClientCallbacks{
     BLERemoteCharacteristic* pGdioCharacteristic = nullptr;
 
     void keyGen(uint8_t *key, uint8_t keyLen, uint8_t seedPin);
-    uint8_t private_key[32];
-    uint8_t public_key[32];
+    // uint8_t localPrivateKey[32];
+    // uint8_t localPublicKey[32];
 };
 
 // class MyClientCallback: public BLEClientCallbacks {
