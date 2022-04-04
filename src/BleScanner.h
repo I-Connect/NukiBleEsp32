c7d6de2302c26699b2030d2ab9702052a993c69b
technyon <j.o.schuemann@gmx.de>
2022-04-04 22:37:40 +0200

    make scan duration configurable

--- src/BleScanner.h
+++ src/BleScanner.h
@@ -19,6 +19,7 @@ class BleScanner : public BLEScannerPublisher, BLEAdvertisedDeviceCallbacks {
 
     void initialize(const std::string& deviceName = "blescanner", const bool wantDuplicates = false, const uint16_t interval = 40, const uint16_t window = 40);
     void update();
+    void setScanDuration(const value);
 
     void subscribe(BLEScannerSubscriber* subscriber) override;
     void unsubscribe(BLEScannerSubscriber* subscriber) override;
@@ -26,6 +27,7 @@ class BleScanner : public BLEScannerPublisher, BLEAdvertisedDeviceCallbacks {
     void onResult(NimBLEAdvertisedDevice* advertisedDevice) override;
 
   private:
+    uint32_t scanDuration = 3;
     BLEScan* bleScan = nullptr;
     std::vector<BLEScannerSubscriber*> subscribers;
 };
