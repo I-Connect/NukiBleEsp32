f9d2ed5b3b199ab44bd648ce75c2e738a5c8db9d
Artem Sheremet <dot.doom@gmail.com>
2024-05-20 10:40:50 +0200

    Initialize errorCode to some non-random value
    Retrieving last error at the wrong time (e.g. before it
    is set from the response) yields rather exotic results,
    as the code is random in RAM.

--- src/NukiBle.cpp
+++ src/NukiBle.cpp
@@ -40,7 +40,8 @@ NukiBle::NukiBle(const std::string& deviceName,
     deviceServiceUUID(deviceServiceUUID),
     gdioUUID(gdioUUID),
     userDataUUID(userDataUUID),
-    preferencesId(preferencedId) {
+    preferencesId(preferencedId),
+    errorCode(ErrorCode::ERROR_UNKNOWN) {
 }
 
 NukiBle::~NukiBle() {
